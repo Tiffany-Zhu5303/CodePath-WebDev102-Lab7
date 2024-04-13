642c3ae72ac8002b2c06e09ff3039f38412a1079
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-04-12 20:31:22 -0400

    changes reverted

--- client/src/client.js
+++ client/src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = "https://quhcfogbjfjggbtevztl.supabase.co";
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1aGNmb2diamZqZ2didGV2enRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5Njc3MDksImV4cCI6MjAyODU0MzcwOX0.yEMFvN1qpLmV3LQbl2l2kA-_xfW74-6E3KkIBDkl5x8";
+const URL = "https://gfsnrakuywzcbtzyuwfj.supabase.co";
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdmc25yYWt1eXd6Y2J0enl1d2ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI0Mjc1NTcsImV4cCI6MjAyODAwMzU1N30.JEAvz6cEHU2NrIbX2ca5i31rfFzhvxqiNaNwStjoTuI";
 export const supabase = createClient(URL, API_KEY);
